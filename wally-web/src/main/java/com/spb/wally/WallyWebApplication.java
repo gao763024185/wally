44a1c3a563da26da46ea89bf01f09157cae95593
gaobaohong <763024185@qq.com>
2018-12-25 18:08:20 +0800

    'springboot结构大改造'

---
+++ wally-web/src/main/java/com/spb/wally/WallyWebApplication.java
@@ -0,0 +1,24 @@
+package com.spb.wally;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 作用：
+ * 1、配置（主要的Spring配置类）
+ * 2、启动引导类
+ * '@SpringBootApplication' 开启了Spring的组件扫描和Spring Boot的自动配置
+ * --'@Configuration'：使用基于Java的配置，而不是XML配置；
+ * 如果需要其他自定义配置可以将其写到一个单独的带有这个注解类里（@ComponentScan会进行扫描）
+ * --'@ComponentScan'：启用组件扫描
+ * --'@EnableAutoConfiguration'	：开启了Spring Boot自动配置的魔力
+ * main----负责启动引导应用程序
+ */
+@SpringBootApplication
+public class WallyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallyWebApplication.class, args);
+    }
+}
--- wally-web/src/test/java/com/spb/wally/WallyWebApplicationTests.java
+++ wally-web/src/test/java/com/spb/wally/WallyWebApplicationTests.java
@@ -1,10 +1,13 @@
-package com.spb.wally.web;
+package com.spb.wally;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * '@SpringApplicationConfiguration'  在1.4.0以后就过期了，被@SpringBootTest取代
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class WallyWebApplicationTests {
